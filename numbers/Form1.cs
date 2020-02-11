02ff6da33f6e5d64cb5253c5d9bf72cef4160850
Kavan Burgess <rc0037072@student.riversidecollege.ac.uk>
2020-02-11 12:43:42 +0000

    gdfg

--- numbers/Form1.cs
+++ numbers/Form1.cs
@@ -27,13 +27,28 @@ namespace numbers
 
             double pi = 3.141592653589793;
             double c = 299792458;
-            richTextBox1.AppendText(pi.ToString());
-            richTextBox1.AppendText(3.ToString());
+            richTextBox1.AppendText(pi.ToString() + "\n");
+            richTextBox1.AppendText(3.ToString() + "\n");
+            richTextBox1.AppendText(pi.ToString("N3") + "\n");
+            richTextBox1.AppendText(pi.ToString("N5") + "\n");
+            richTextBox1.AppendText(pi.ToString("N15") + "\n");
+            richTextBox1.AppendText(c.ToString("N0") + "\n");
+            richTextBox1.AppendText(c.ToString("0.00E0") + "\n");
+            richTextBox1.AppendText(c.ToString("0.000000000E0") + "\n");
 
-        }
+
+                
+
+            }
+        
 
         private void RichTextBox1_TextChanged(object sender, EventArgs e)
         {
+
+            string s = "3";
+            string pi = "3.14159265";
+            int i = int.Parse(s);
+            double d = double.Parse(pi);
 
         }
     }
